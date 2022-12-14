7b14c71e0f0505a885d0935b4c78fd6bc1ae25d9
Vlad <vladot@ya.ru>
2022-12-14 16:32:07 +0600

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-
+        task1();
+//        task2();
+//        task3();
     }
+
+    public static void task1() {
+
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        System.out.println();
+        System.out.println();
+    }
+
 }
